da31dd2147d7947c5a5d8f65f63200610b77dae0
jiaxincheng <jiaxincheng@jiaxinchengdeMac-mini.local>
2026-03-12 20:18:06 +0800

    Remove obsolete overfit configs

--- overfit.py
+++ overfit.py
@@ -7,7 +7,7 @@ from train import main as train_main
 def main() -> None:
     parser = argparse.ArgumentParser(description='Run a focused overfit experiment for the parametric edge detector.')
     parser.add_argument('--config', default='configs/parametric_edge/default.yaml')
-    parser.add_argument('--override-config', default='configs/parametric_edge/overfit.yaml')
+    parser.add_argument('--override-config', default='configs/parametric_edge/overfit_rgb_dinov2.yaml')
     args = parser.parse_args()
     sys.argv = [sys.argv[0], '--config', args.config, '--override-config', args.override_config]
     train_main()
